f2f47613f93866debbe19900e5c718510cb600be
Neenu-nonu <neenu.dsqre@yahoo.com>
2016-06-02 15:38:34 +0530

    dropdown

--- workshop/urls.py
+++ workshop/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from registeration.views import Home
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', Home.as_view(), name='home'),
+    url(r'^admin/',admin.site.urls),
+
+
 ]
